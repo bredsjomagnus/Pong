5bdc291b053525ab333db8bd1bb14c42de7d6897
Magnus Andersson <bredsjomagnus@yahoo.se>
2019-12-01 10:49:54 +0100

    Score

--- pong.py
+++ pong.py
@@ -3,14 +3,16 @@ import os
 pygame.mixer.init(22100, -16, 2, 256)
 pygame.init()
 
-background_image = pygame.image.load("images/bakgrund-VS.jpg")
+background_image = pygame.image.load("images/backgrounds/bakgrund-VS.jpg")
 
-windowsize = (400,230)
+# windowsize = (pygame.display.Info().current_w-100, pygame.display.Info().current_h-100)
+windowsize = (1000,636)
+# print(pygame.display.Info())
+
 win = pygame.display.set_mode(windowsize)
 pygame.display.set_caption("PONG - the classic game")
 pygame.key.set_repeat(0, 500)
 bounce = pygame.mixer.Sound(os.path.join('sounds','pool.ogg'))
-
 
 def play_bounce(direction):
     empty_channel = pygame.mixer.find_channel()
@@ -72,36 +74,59 @@ class GameObject:
 class Ball(GameObject):
     def drawMe(self,surface):
         pygame.draw.circle(surface,self.color,(round(self.getX()),round(self.getY())),round(self.getWidth()/2.0))
+
+class Status:
+    statuscoords = (0,0)
+    dimensions = (0,0)
+    bgcolor = (0,0,0)
+    me = pygame.image.load(os.path.join('images/backgrounds', 'field_black_1000x636.png'))
+
+    def __init__(self, statuscoords=(300,0), dimensions=(100,200), bgcolor=(0,0,0)):
+        self.statuscoords = statuscoords
+        self.dimensions = dimensions
+        self.bgcolor = bgcolor
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.bgcolor, [self.coords[0],self.coords[1],self.dimensions[0],self.dimensions[1]])
+
+    def draw(self,surface):
+        surface.blit(self.me, (self.statuscoords[0],self.statuscoords[1]))
+        #return self.me
+
+    def coords(self):
+        return (self.statuscoords[0],self.statuscoords[1])
+
 class StatusContent:
     textArray = []
     size = 12
-    def __init__(self, text="",size=12,clor = (255,255,255),coords=(0,0),bold=False,italic=False):
+    def __init__(self, text="",size=12,color = (255,255,255),coords=(0,0),bold=False,italic=False):
         self.setText(text)
         self.size = size
         self.color = color
         self.statcoords = coords
         self.bold = bold
         self.italic = italic
-        self.fontobj = pygame.front.SysFront("sans serif",self.size,self.bold,self.italic)
+        self.fontobj = pygame.font.SysFont("sans serif",self.size,self.bold,self.italic)
 
     def draw(self,surface):
         n = 0
         for text in self.textArray:
-            TextSurface = self.fontobj.render(text, true, self.color)
-            Surface.blit(TextSurface, (self.statcoords[0],self.statcoords[1]+n*self.size))
+            TextSurface = self.fontobj.render(text, True, self.color)
+            surface.blit(TextSurface, (self.statcoords[0],self.statcoords[1]+n*self.size))
             n += 1
 
-            def coords(self):
-                return self.statcoords
-            def setText(self,text):
-                if text.find("\n") > -1:
-                    self.textArray = text.split("\n")
-                    n = 0
-                    for text in self.textArray:
-                        self.textArray[n] = text.strip()
-                        n += 1
-                else:
-                   self.textArray = [text]
+    def coords(self):
+        return self.statcoords
+    
+    def setText(self,text):
+        if text.find("\n") > -1:
+            self.textArray = text.split("\n")
+            n = 0
+            for text in self.textArray:
+                self.textArray[n] = text.strip()
+                n += 1
+        else:
+            self.textArray = [text]
 
 class GameRules():
     def __init__(self,ball,walls,paddles,gamestate = 0):
@@ -123,9 +148,10 @@ class GameRules():
                     self.ball.setCoords(((self.walls[1].getX()+self.walls[1].getWidth())/2.0,(self.walls[1].getY()+self.walls[1].getHeight())/2.0))
                     if(n==1):
                         self.score1=self.score1+1
+                        
                     else:
                         self.score2=self.score2+1
-                    
+                        
                 else:
                     Ycoord = [wall.getY()+wall.getHeight(),0,wall.getY()-self.ball.getHeight(),0]
                     ball.setCoords((ball.getX(),Ycoord[n]))
@@ -143,6 +169,9 @@ class GameRules():
                     play_bounce('right')
                     ball.setCoords((paddle.getX()-self.ball.getWidth(),self.ball.getY()))
             n += 1
+    
+    def getScore(self):
+        return str(self.score1), str(self.score2)
                 
     def keyPressed(self, keys):
         available_keys = self.p1Keys+self.p2Keys
@@ -168,16 +197,26 @@ class GameRules():
             obj.drawMe(surface)
         
         
-ball = Ball(color=(255,0,0),dimensions=(10,10),coords=(windowsize[0]/2.0,windowsize[1]/2.0),speed=(0.05,0.05))
+ball = Ball(color=(255,0,0),dimensions=(10,10),coords=(windowsize[0]/2.0,windowsize[1]/2.0),speed=(1.75,1.75))
 walls = []
 walls.append(GameObject(color=(255,255,0),dimensions=(windowsize[0],1),coords=(0,0)))   #top, right, bottom, left
 walls.append(GameObject(color=(255,255,0),dimensions=(1,windowsize[1]),coords=(windowsize[0]-1,0)))
 walls.append(GameObject(color=(255,255,0),dimensions=(windowsize[0],1),coords=(0,windowsize[1]-1)))
 walls.append(GameObject(color=(255,255,0),dimensions=(1,windowsize[1]),coords=(0,0)))
 paddles = []
-paddles.append(GameObject(color=(0,0,255),dimensions=(10,40),coords=(0,windowsize[1]/2.0),speed=(0.0,0.07)))  #player1, player2
-paddles.append(GameObject(color=(0,255,0),dimensions=(10,40),coords=(windowsize[0]-10,windowsize[1]/2.0),speed=(0.0,0.07)))
+paddles.append(GameObject(color=(0,0,255),dimensions=(10,40),coords=(0,windowsize[1]/2.0),speed=(0.0,1.57)))  #player1, player2
+paddles.append(GameObject(color=(0,255,0),dimensions=(10,40),coords=(windowsize[0]-10,windowsize[1]/2.0),speed=(0.0,1.57)))
 rules = GameRules(ball,walls,paddles,1)
+
+theStatus = Status((0,0),(1000,636),(0,0,0))
+
+print("score1:")
+print(rules.getScore()[0])
+
+theStatusContent = {
+    "score1":   StatusContent(text=rules.getScore()[0], size=78, coords=(250,300)),
+    "score2":   StatusContent(text=rules.getScore()[1], size=78, coords=(750,300)),
+}
 
 run = True
 while run:
@@ -193,8 +232,13 @@ while run:
     rules.bounce()
     ball.move()
 
-    # win.fill((0,0,0))
-    win.blit(background_image, [0,0])
+    win.fill((0,0,0))
+    # win.blit(background_image, [0,0])
+    theStatusContent["score1"].setText(rules.getScore()[0])
+    theStatusContent["score2"].setText(rules.getScore()[1])
+    theStatus.draw(win)
+    for v in theStatusContent.values():
+        v.draw(win)
     rules.draw(win)
     pygame.display.update()
 
