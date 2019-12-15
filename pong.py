c1daa359e7e3e4de507eb97d507a7131b2c73cde
PihlFredrik <fredrik.fogelqvist@pihlskolan.hellefors.se>
2019-12-15 20:04:58 +0100

    Reset (inte pause)
    Splash screen, reset button, random angle faceoff

--- pong.py
+++ pong.py
@@ -1,5 +1,7 @@
 import pygame
 import os
+import math
+import random
 pygame.mixer.init(16000, -16, 2, 2048)
 pygame.init()
 
@@ -30,11 +32,12 @@ def play_wall_bounce():
 
 def play_bounce(direction):
     empty_channel = pygame.mixer.find_channel()
-    if direction == 'left':
-        empty_channel.set_volume(1.0,0.0)
-    else:
-        empty_channel.set_volume(0.0,1.0)
-    empty_channel.play(bounce)
+    if(empty_channel != None):
+        if direction == 'left':
+            empty_channel.set_volume(1.0,0.0)
+        else:
+            empty_channel.set_volume(0.0,1.0)
+        empty_channel.play(bounce)
 
 class GameObject:
     def __init__(self,color=(0,0,0),dimensions=(10,10),coords=(0,0), speed=(0.0,0.0)):
@@ -45,16 +48,19 @@ class GameObject:
 
     def getSpeedX(self):
         return self.speed[0]
-  
+
     def getSpeedY(self):
         return self.speed[1]
-  
+
+    def getSpeed(self):
+        return math.sqrt(self.speed[0]**2+self.speed[1]**2) #pythagoras sats <3
+
     def setSpeed(self,speed):
         self.speed = speed
-  
+
     def getX(self):
         return self.coords[0]
-  
+
     def getY(self):
         return self.coords[1]
 
@@ -66,10 +72,10 @@ class GameObject:
 
     def getHeight(self):
         return self.dimensions[1]
-  
+
     def drawMe(self,surface):
         pygame.draw.rect(surface, self.color, pygame.Rect(round(self.getX()), round(self.getY()), self.getWidth(), self.getHeight()))
-  
+
     def move(self):
         coords = list(self.coords)
         coords[0] = self.coords[0] + self.speed[0]
@@ -89,22 +95,32 @@ class Ball(GameObject):
     def drawMe(self,surface):
         pygame.draw.circle(surface,self.color,(round(self.getX()),round(self.getY())),round(self.getWidth()/2.0))
 
+    def randomAngle(self, wall=None):
+        angle = random.uniform(0.17,math.pi-0.17) #random vinkel mellan 10-170 (omvandlat till radianer)
+        if(wall == 1):  #förskjut vinkeln så att tekningen går mot angiven vägg; höger
+            angle = angle - math.pi/2.0
+        else:           #förskjut vinkeln så att tekningen går mot angiven vägg; vänster
+            angle = angle + math.pi/2.0
+        self.setAngle(angle)
 
-
-
-
+    def setAngle(self,angle):
+        newSpeed = (self.getSpeed()*math.cos(angle),self.getSpeed()*math.sin(angle))
+        self.setSpeed(newSpeed)
 
 
 class Status:
     statuscoords = (0,0)
     dimensions = (0,0)
     bgcolor = (0,0,0)
-    me = pygame.image.load(os.path.join('images/backgrounds', 'field_black_1320x740.png'))
 
-    def __init__(self, statuscoords=(300,0), dimensions=(100,200), bgcolor=(0,0,0)):
+    def __init__(self, statuscoords=(300,0), dimensions=(100,200), bgcolor=(0,0,0), backgroundfile=None):
         self.statuscoords = statuscoords
         self.dimensions = dimensions
         self.bgcolor = bgcolor
+        if(backgroundfile == None):
+            self.me = pygame.image.load(os.path.join('images/backgrounds', 'field_black_1320x740.png'))
+        else:
+            self.me = pygame.image.load(os.path.join('images/backgrounds', backgroundfile))
 
     # def draw(self, surface):
     #     pygame.draw.rect(surface, self.bgcolor, [self.coords[0],self.coords[1],self.dimensions[0],self.dimensions[1]])
@@ -115,11 +131,6 @@ class Status:
 
     def coords(self):
         return (self.statuscoords[0],self.statuscoords[1])
-
-
-
-
-
 
 
 
@@ -144,7 +155,7 @@ class StatusContent:
 
     def coords(self):
         return self.statcoords
-    
+
     def setText(self,text):
         if text.find("\n") > -1:
             self.textArray = text.split("\n")
@@ -156,6 +167,10 @@ class StatusContent:
             self.textArray = [text]
 
 class GameRules():
+    """
+    Gamestate = 0 --> SplashScreen
+    Gamestate = 1 --> Game on!
+    """
     def __init__(self,ball,walls,paddles,gamestate = 0):
         self.score1=0
         self.score2=0
@@ -165,20 +180,19 @@ class GameRules():
         self.p1Keys = [pygame.K_w,pygame.K_s]   #keys as up,down
         self.p2Keys = [pygame.K_i,pygame.K_k]
         self.gamestate = gamestate
-        
+
     def bounce(self):
         n = 0
         for wall in self.walls:
             collideX, collideY = self.ball.collides(wall)
             if(collideX and collideY):
                 if(n==1 or n==3):    #if walls right or left don´t bounce - do score thingy
-                    self.ball.setCoords(((self.walls[1].getX()+self.walls[1].getWidth())/2.0,(self.walls[1].getY()+self.walls[1].getHeight())/2.0))
                     if(n==1):
                         self.score1=self.score1+1
-                        
+                        self.faceOff(3)
                     else:
                         self.score2=self.score2+1
-                        
+                        self.faceOff(1)
                 else:
                     play_wall_bounce()
                     Ycoord = [wall.getY()+wall.getHeight(),0,wall.getY()-self.ball.getHeight(),0]
@@ -192,15 +206,28 @@ class GameRules():
                 self.ball.setSpeed(((-1)*self.ball.getSpeedX(),self.ball.getSpeedY()))
                 if(n==0):
                     play_bounce('left')
-                    ball.setCoords((paddle.getX()+paddle.getWidth(),self.ball.getY()))
+                    self.ball.setCoords((paddle.getX()+paddle.getWidth(),self.ball.getY()))
                 else:
                     play_bounce('right')
-                    ball.setCoords((paddle.getX()-self.ball.getWidth(),self.ball.getY()))
+                    self.ball.setCoords((paddle.getX()-self.ball.getWidth(),self.ball.getY()))
             n += 1
-    
+
+    def faceOff(self,wall=None):
+        if(wall == None):
+            wall = random.randint(1,2)     #egentligen ska wall vara 1 eller 3, men if-satsen nedan bryr sig bara om ena värdet, så två funkar lika bra.
+        self.ball.setCoords(((self.walls[1].getX()+self.walls[1].getWidth())/2.0,(self.walls[1].getY()+self.walls[1].getHeight())/2.0))
+        self.ball.randomAngle(wall)
+
+    def resetGame(self):
+        self.faceOff()
+        self.score1 = 0
+        self.score2 = 0
+        self.paddles[0].setCoords((5,windowsize[1]/2.0))  #player1, player2
+        self.paddles[1].setCoords((windowsize[0]-15,windowsize[1]/2.0))
+
     def getScore(self):
         return str(self.score1), str(self.score2)
-                
+
     def keyPressed(self, keys):
         available_keys = self.p1Keys+self.p2Keys
         for key in available_keys:
@@ -219,27 +246,26 @@ class GameRules():
                 collideX, collideY = self.paddles[p].collides(self.walls[2])
                 if(collideY):
                     self.paddles[p].setCoords((self.paddles[p].getX(),self.walls[2].getY()-self.paddles[p].getHeight()))
-                
+
     def draw(self,surface):
         for obj in walls+paddles+[ball]:
             obj.drawMe(surface)
-        
-        
-ball = Ball(color=(255,0,0),dimensions=(10,10),coords=(windowsize[0]/2.0,windowsize[1]/2.0),speed=(10,10))
+
+
+ball = Ball(color=(255,0,0),dimensions=(10,10),coords=(windowsize[0]/2.0,windowsize[1]/2.0),speed=(5,5))
 walls = []
-walls.append(GameObject(color=(255,255,0),dimensions=(windowsize[0],1),coords=(0,0)))   #top, right, bottom, left
-walls.append(GameObject(color=(255,255,0),dimensions=(1,windowsize[1]),coords=(windowsize[0]-1,0)))
-walls.append(GameObject(color=(255,255,0),dimensions=(windowsize[0],1),coords=(0,windowsize[1]-1)))
-walls.append(GameObject(color=(255,255,0),dimensions=(1,windowsize[1]),coords=(0,0)))
+walls.append(GameObject(color=(255,255,0),dimensions=(windowsize[0],100),coords=(0,-99)))   #top, right, bottom, left
+walls.append(GameObject(color=(255,255,0),dimensions=(100,windowsize[1]),coords=(windowsize[0]-1,0)))
+walls.append(GameObject(color=(255,255,0),dimensions=(windowsize[0],100),coords=(0,windowsize[1]-1)))
+walls.append(GameObject(color=(255,255,0),dimensions=(100,windowsize[1]),coords=(-99,0)))
 paddles = []
 paddles.append(GameObject(color=(0,0,255),dimensions=(15,80),coords=(5,windowsize[1]/2.0),speed=(0.0,5.57)))  #player1, player2
 paddles.append(GameObject(color=(0,255,0),dimensions=(15,80),coords=(windowsize[0]-15,windowsize[1]/2.0),speed=(0.0,5.57)))
-rules = GameRules(ball,walls,paddles,1)
+rules = GameRules(ball,walls,paddles,0)
+rules.faceOff()
 
+theSplash = Status((0,0),(1320,768),(0,0,0),"splash.png")
 theStatus = Status((10,10),(1300,720),(0,0,0))
-
-print("score1:")
-print(rules.getScore()[0])
 
 theStatusContent = {
     "score1":   StatusContent(text=rules.getScore()[0], size=78, coords=(330,370)),
@@ -254,21 +280,29 @@ while run:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 run = False
-        pygame.event.pump()
+            if event.key == pygame.K_RETURN:
+                if(rules.gamestate == 0):
+                    rules.gamestate = 1
+                    rules.resetGame()
+                else:
+                    rules.gamestate = 0
 
-    rules.keyPressed(pygame.key.get_pressed())
-    rules.bounce()
-    ball.move()
+        pygame.event.pump()
 
     win.fill((0,0,0))
     # win.blit(background_image, [0,0])
-    theStatusContent["score1"].setText(rules.getScore()[0])
-    theStatusContent["score2"].setText(rules.getScore()[1])
-    theStatus.draw(win)
-    for v in theStatusContent.values():
-        v.draw(win)
-    rules.draw(win)
+    if(rules.gamestate == 0):
+        theSplash.draw(win)
+    else:
+        rules.keyPressed(pygame.key.get_pressed())
+        rules.bounce()
+        ball.move()
+        theStatusContent["score1"].setText(rules.getScore()[0])
+        theStatusContent["score2"].setText(rules.getScore()[1])
+        theStatus.draw(win)
+        for v in theStatusContent.values():
+            v.draw(win)
+        rules.draw(win)
     pygame.display.update()
 
 pygame.quit()
-    
